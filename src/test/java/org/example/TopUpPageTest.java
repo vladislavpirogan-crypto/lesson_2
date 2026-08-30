ecf9dfddac8e767b0e398bf1fe7eb70e67bf62f2
Vladislav <vladislavpirogan@gmail.com>
2026-08-30 14:10:48 +0700

    wip: мои правки для  (TopUpPageTest и PageObject классы)

--- src/test/java/org/example/TopUpPageTest.java
+++ src/test/java/org/example/TopUpPageTest.java
@@ -1,4 +1,58 @@
 package org.example;
 
+import org.example.pages.PaymentDetailsPage;
+import org.example.pages.CardDetailsPage;
+import org.example.pages.TopUpPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertNotNull;
+
 public class TopUpPageTest {
+
+    private WebDriver driver;
+
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://mts.by");
+    }
+
+    @Test
+    public void testTopUpFlow() {
+        PaymentDetailsPage paymentPage = new PaymentDetailsPage(driver);
+        paymentPage.enterAmount("500");
+        paymentPage.clickContinue();
+
+        String summary = paymentPage.getSummaryAmountText();
+        assertNotNull(summary, "Сумма не отобразилась");
+
+        CardDetailsPage cardPage = new CardDetailsPage(driver);
+        cardPage.enterCardNumber("4111111111111111");
+        cardPage.enterExpiryDate("12/25");
+        cardPage.enterCvv("123");
+
+        String iconsText = cardPage.getPaymentIconsText();
+        assertNotNull(iconsText, "Иконки платежных систем не найдены");
+
+
+    }
+
+
+    @Test
+    public void shouldSelectPaymentType() {
+        // Создаём объект страницы
+        TopUpPage topUpPage = new TopUpPage(driver);
+
+        // Вызываем метод выбора типа платежа
+        topUpPage.selectPaymentType("Internet");
+
+        // Вызываем метод получения плейсхолдера
+        String placeholder = topUpPage.getPlaceholderText("amount");
+
+        // Проверка: результат не должен быть null
+        assertNotNull(placeholder, "Плейсхолдер не должен быть null");
+    }
 }
